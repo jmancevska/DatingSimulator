c2a8bafb63312283c3be9e27a59a6a568ca3c3f1
jmancevska <jmancevska@users.noreply.github.com>
2016-12-07 00:26:46 -0700

    Initial version

---
+++ target.py
@@ -0,0 +1,24 @@
+import random
+class Target:
+      xInteractions = 0
+      yInteractions = 0
+      probabilityOfX = 0
+      probabilityOfY = 0
+
+      def __init__(self, probabilityOfX, probabilityOfY):
+          self.probabilityOfX = probabilityOfX
+          self.probabilityOfY = probabilityOfY
+
+      def displayReport(self):
+        print('Number of X interactions: ', self.xInteractions)
+        print('Number of Y interactions: ', self.yInteractions)
+
+      def generateInteraction(self):
+        interactions = { 'X': self.probabilityOfX, 'Y': self.probabilityOfY }
+        chosen = random.choice([k for k in interactions for dummy in range(interactions[k])])
+        if chosen == 'X':
+          self.xInteractions = self.xInteractions + 1
+        else:
+          self.yInteractions = self.yInteractions + 1
+          
+        return chosen
